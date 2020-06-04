534bc6a18237d5753ddfd68c014553b1e037de61
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-04 11:57:12 +0530

    Configured routes

--- Nodejs/mongodb-node-1/app.js
+++ Nodejs/mongodb-node-1/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+
+
+app.use(express.json());
+
+const userRoutes = require("./routes/userRoutes");
+const errorHandler = require('./controllers/errorHandler');
+
+app.use('/api/v1/users', userRoutes);
+app.use(errorHandler);
+
 module.exports = app;
