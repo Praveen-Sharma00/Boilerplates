53418e416dcdcb8becee2a86eb279a44fe5916e6
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-04 12:20:10 +0530

    Modified 'userModel' to hash password

--- Nodejs/mongodb-node-1/models/userModel.js
+++ Nodejs/mongodb-node-1/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,10 +22,21 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please provide a confirm password']
+        required: [true, 'Please provide a confirm password'],
+        validate: {
+            validator(el) {
+                return this.password === el;
+            },
+            message: 'Passwords are not same'
+        }
     }
 })
-
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+})
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
