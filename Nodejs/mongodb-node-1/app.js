8595c53ca2928fc9b6fb2cf2706ee74249c7332a
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-05 10:39:59 +0530

    Added 404 handler

--- Nodejs/mongodb-node-1/app.js
+++ Nodejs/mongodb-node-1/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+const AppError = require('./utils/appError');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.all('*', (req, res, next) => {
+    next(new AppError('Server cannot find the requested page'));
+})
 module.exports = app;
