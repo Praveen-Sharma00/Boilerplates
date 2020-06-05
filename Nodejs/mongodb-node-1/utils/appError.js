d615052ced156e24ebbf21658a3dbd0c1ac563a8
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-05 10:39:41 +0530

    Finished ErrorHandling utils

--- Nodejs/mongodb-node-1/utils/appError.js
+++ Nodejs/mongodb-node-1/utils/appError.js
@@ -3,8 +3,11 @@ class AppError extends Error {
         super();
         this.message = message;
         this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
 
-        Error.captureStackTrace(this);
+        Error.captureStackTrace(this, this.constructor);
     }
 }
+
+module.exports = AppError;
