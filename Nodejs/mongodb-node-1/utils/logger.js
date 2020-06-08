fc5025da3d6b48f9b61ddcdfc146b527b4126020
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-06-08 10:51:57 +0530

    Removed all files

--- Nodejs/mongodb-node-1/utils/logger.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-
-const logger = console.log
-global.logError = (text) => logger(chalk.red.bold(text));
-global.logInfo = (text) => logger(chalk.blueBright.bold(text));
-global.logSuccess = (text) => logger(chalk.greenBright.bold(text));
