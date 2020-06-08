3e3ed29dc546f261b06f56e6da5a15bcb715a727
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-06-08 14:06:08 +0530

    Formatted the code

--- Nodejs/mongodb-node-1/controllers/errorController.js
+++ Nodejs/mongodb-node-1/controllers/errorController.js
@@ -2,42 +2,42 @@ const AppError = require('../utils/appError')
 
 const handleCastError = (error) => {
     const message = `Invalid ${error.path} : ${error.value}.`
-    return new AppError(message,400)
+    return new AppError(message, 400)
 }
 
 const handleValidationError = (error) => {
-    const errors = Object.values(error.errors).map(e=>e.message)
+    const errors = Object.values(error.errors).map(e => e.message)
     const message = `Invalid input data ${errors.join('. ')}.`
-    return new AppError(message,400)
+    return new AppError(message, 400)
 }
 
 const handleDuplicateError = (error) => {
     const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
     const message = `Duplicate field value : ${value}. Please use another `
-    return new AppError(message,400)
+    return new AppError(message, 400)
 }
 
 
 const sendDevError = (err, res) => {
     return res
-            .status(err.statusCode)
-            .json({
-                status: err.status,
-                message: err.message, 
-                stack: err.stack,
-            })
+        .status(err.statusCode)
+        .json({
+            status: err.status,
+            message: err.message,
+            stack: err.stack,
+        })
 }
 const sendProdError = (err, res) => {
-    if(err.isOperational){
+    if (err.isOperational) {
         res.status(err.statusCode)
-        .json({
-            message:err.message,
-            status:err.status
-        })
-    }else{
+            .json({
+                message: err.message,
+                status: err.status
+            })
+    } else {
         res.status(500).json({
-            message:'Error occurred on server',
-            status:'error'
+            message: 'Error occurred on server',
+            status: 'error'
         })
     }
 }
@@ -52,12 +52,12 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendDevError(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        let error = {...err}
-        if(err.name === 'ValidationError') 
+        let error = { ...err }
+        if (err.name === 'ValidationError')
             error = handleValidationError(err)
-        else if(err.name === 'CastError')
+        else if (err.name === 'CastError')
             error = handleCastError(err)
-        else if(err.code === 11000)
+        else if (err.code === 11000)
             error = handleDuplicateError(err)
 
         sendProdError(err, res)
