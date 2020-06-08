fe4ef6943918a057496db321a0918c49aa4d7979
Praveen Sharma <praveensharma.inbox@gmail.com>
2020-06-08 12:03:50 +0530

    Setup error handlers

--- Nodejs/mongodb-node-1/app.js
+++ Nodejs/mongodb-node-1/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+const AppError = require('./utils/appError')
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.all('*', (req, res ,next)=>{
+    const err= new AppError('Could not find the page being requested !')
+    next(err)
+})
 
 module.exports = app
