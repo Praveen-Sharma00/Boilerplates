76b45ff2e379d86584f65d8152b3aa6fcb22342d
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-09 10:51:13 +0530

    Added UserModel

---
+++ Nodejs/mongodb-node-1/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email !']
+    },
+    photo: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    passwordConfirm: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
