6db29bc8b9c3b46f5dd00225521676dfa8f0fde1
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-10 06:13:13 +0530

    Formatted code

--- Nodejs/mongodb-node-1/app.js
+++ Nodejs/mongodb-node-1/app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 
+const authRouter = require('./routes/authRoutes')
+
 const AppError = require('./utils/appError')
 const errorController = require('./controllers/errorController')
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.all('*', (req, res ,next)=>{
-    const err= new AppError('Could not find the page being requested !',400)
+app.use(authRouter)
+
+app.all('*', (req, res, next) => {
+    const err = new AppError('Could not find the page being requested !', 400)
     next(err)
 })
 
