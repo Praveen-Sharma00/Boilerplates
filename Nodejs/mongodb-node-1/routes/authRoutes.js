0eac85e2e4e673db40b983e79b66daff26f8a472
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-10 20:03:02 +0530

    Added 'login' route

--- Nodejs/mongodb-node-1/routes/authRoutes.js
+++ Nodejs/mongodb-node-1/routes/authRoutes.js
@@ -7,4 +7,7 @@ const authController = require('../controllers/authController')
 router.route('/signup')
     .post(authController.signup)
 
+router.route('/login')
+    .post(authController.login)
+
 module.exports = router
