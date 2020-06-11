65a9bad9da316ed5d700583705a9373a752d7022
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-11 12:27:57 +0530

    Added Reset/Forgot password route

--- Nodejs/mongodb-node-1/routes/userRoutes.js
+++ Nodejs/mongodb-node-1/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
 const authenticateUser = require('../middlewares/authenticateUser')
 const authoriseUser = require('../middlewares/authoriseUser')
@@ -14,5 +15,7 @@ router.route('/settings')
         authoriseUser.restrictTo('admin'),
         userController.settings)
 
+router.post('/forgot-password', authController.forgotPassword)
+router.patch('/reset-password/:token', authController.resetPassword)
 
 module.exports = router
