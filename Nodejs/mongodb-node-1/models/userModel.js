7e7d005d35793899f5fb3a50b234b29b5e943d04
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-11 12:28:34 +0530

    Implemented  'createPasswordResetToken' static user  method

--- Nodejs/mongodb-node-1/models/userModel.js
+++ Nodejs/mongodb-node-1/models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,7 +41,9 @@ const userSchema = new mongoose.Schema({
             message: "Passwords don't match !"
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function (next) {
@@ -61,8 +65,19 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
         return JWTTimestamp < changedTimeStamp
     }
-
 }
 
+userSchema.methods.createPasswordResetToken = function () {
+
+    const resetToken = crypto.randomBytes(32).toString('hex')
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+
+    return resetToken
+}
 const User = mongoose.model('User', userSchema)
 module.exports = User
