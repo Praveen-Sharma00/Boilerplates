dae45d2b8c86e0256c5fc67297489f09e88b7e7a
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-11 10:46:26 +0530

    Implemented authorization

--- Nodejs/mongodb-node-1/controllers/userController.js
+++ Nodejs/mongodb-node-1/controllers/userController.js
@@ -4,4 +4,10 @@ exports.home = asyncHandler((req, res, next) => {
     return res.json({
         user: req.user
     })
-}) 
+})
+
+exports.settings = asyncHandler((req, res, next) => {
+    return res.json({
+        message: 'settings working !'
+    })
+})
--- Nodejs/mongodb-node-1/routes/userRoutes.js
+++ Nodejs/mongodb-node-1/routes/userRoutes.js
@@ -2,10 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+
 const authenticateUser = require('../middlewares/authenticateUser')
+const authoriseUser = require('../middlewares/authoriseUser')
 
 router.route('/home')
     .get(authenticateUser, userController.home)
+
+router.route('/settings')
+    .get(authenticateUser,
+        authoriseUser.restrictTo('admin'),
+        userController.settings)
 
 
 module.exports = router
