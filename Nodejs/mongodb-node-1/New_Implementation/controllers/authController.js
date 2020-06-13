4ae534e57a691dff5bc38130d7fc9f019baff6b0
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-14 00:36:08 +0530

    Auth Controller init done

--- Nodejs/mongodb-node-1/New_Implementation/controllers/authController.js
+++ Nodejs/mongodb-node-1/New_Implementation/controllers/authController.js
@@ -1,1 +1,18 @@
+import * as HttpStatus from 'http-status-codes'
+
+import asyncHandler from '../utils/asyncHandler'
+import Response from '../utils/response'
+
+const registerUser = asyncHandler((req, res, next) => {
+    Response.success(res, 'Route working successfully')
+})
+
+const loginUser = asyncHandler((req, res, next) => {
+    Response.success(res, 'Route working successfully')
+})
+
+export const AuthController = {
+    registerUser, loginUser
+}
+
 
