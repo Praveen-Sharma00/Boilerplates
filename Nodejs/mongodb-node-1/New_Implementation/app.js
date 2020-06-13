101b88866f66a8ec5b8f04acfba2d8e4a8a27944
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-14 00:36:26 +0530

    Fixed middleware issue

--- Nodejs/mongodb-node-1/New_Implementation/app.js
+++ Nodejs/mongodb-node-1/New_Implementation/app.js
@@ -1,24 +1,25 @@
 import express from 'express'
 import helmet from 'helmet'
-import * as rateLimit from 'express-rate-limit'
-import * as xss from 'xss-clean'
-import * as mongoSanitize from 'express-mongo-sanitize'
+import rateLimit from 'express-rate-limit'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
 
-
+import authRoutes from './routes/authRoutes'
 const app = express()
 
-app.use('/api',rateLimit({
-    windowMs:60*60*1000,
-    max:100,
-    message:'Too many request from your IP !'
-}))
-app.use(helmet)
+const limit = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 100,
+    message: 'Too many request from your IP !'
+})
+app.use('/api', limit)
+app.use(helmet())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(`${__dirname}/public`))
 app.use(mongoSanitize())
 app.use(xss())
 
-
+app.use('/api/v1/auth', authRoutes)
 export default app;
 
