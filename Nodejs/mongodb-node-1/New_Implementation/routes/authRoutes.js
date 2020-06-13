ad8647b7ded5719ccf59ee27cae46e52d6d18f0d
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-14 00:35:54 +0530

    Auth routes init done

---
+++ Nodejs/mongodb-node-1/New_Implementation/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+import { AuthController } from '../controllers/authController'
+
+const authRouter = express.Router()
+
+authRouter.route('/register')
+    .post(AuthController.registerUser)
+
+authRouter.route('/login')
+    .post(AuthController.loginUser)
+
+
+export default authRouter
