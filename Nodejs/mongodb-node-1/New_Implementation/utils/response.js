e0a178620b8b1a58fd06db6940d465f0b1008347
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-13 23:11:45 +0530

    Finished Implementing Common 'Response' handler class

---
+++ Nodejs/mongodb-node-1/New_Implementation/utils/response.js
@@ -0,0 +1,21 @@
+import * as HttpStatus from 'http-status-codes'
+import * as _ from 'lodash'
+
+export default class Response {
+    static success(res, message, data = null, code = HttpStatus.OK, extra = {}) {
+        const responseObj = { success: true }
+        responseObj['message'] = message || 'success';
+        responseObj['data'] = data || null;
+        responseObj['statusCode'] = code || HttpStatus.OK;
+        responseObj['extra'] = extra || {};
+
+        res.status(responseObj.statusCode).json(responseObj)
+    }
+
+    static fail(res, message, code = HttpStatus.NOT_FOUND, extra = {}) {
+        const responseObj = { success: false }
+        responseObj['message'] = message || 'failed';
+        responseObj['statusCode'] = code || HttpStatus.NOT_FOUND;
+        responseObj['extra'] = extra || {};
+    }
+}
