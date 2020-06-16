ae8a18a8406fbab990a2916282685ce871bf5937
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-16 09:42:21 +0530

    Added 'error' response method

--- Nodejs/mongodb-node-1/New_Implementation/utils/response.js
+++ Nodejs/mongodb-node-1/New_Implementation/utils/response.js
@@ -3,8 +3,9 @@ import * as _ from 'lodash'
 
 export default class Response {
     static success(res, message, data = null, code = HttpStatus.OK, extra = {}) {
-        const responseObj = { success: true }
-        responseObj['message'] = message || 'success';
+        const responseObj = {success: true}
+        responseObj['status'] = 'success';
+        responseObj['message'] = message || '';
         responseObj['data'] = data || null;
         responseObj['statusCode'] = code || HttpStatus.OK;
         responseObj['extra'] = extra || {};
@@ -13,11 +14,22 @@ export default class Response {
     }
 
     static fail(res, message, code = HttpStatus.NOT_FOUND, extra = {}) {
-        const responseObj = { success: false }
-        responseObj['message'] = message || 'failed';
+        const responseObj = {success: false}
+        responseObj['status'] = 'success';
+        responseObj['message'] = message || '';
         responseObj['statusCode'] = code || HttpStatus.NOT_FOUND;
         responseObj['extra'] = extra || {};
 
         res.status(responseObj.statusCode).json(responseObj)
+    }
+
+    static error(res, message, errorObj, code = HttpStatus.NOT_FOUND) {
+        res.status(code).json({
+            status: 'error',
+            code: code,
+            message: message || '',
+            error:{...errorObj},
+            // val : Object.values(errorObj.errors)
+        })
     }
 }
