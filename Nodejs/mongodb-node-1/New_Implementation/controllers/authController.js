15971dd644f66c9d4e4296679162228183fdf578
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-16 09:43:29 +0530

    Started work on AuthController

--- Nodejs/mongodb-node-1/New_Implementation/controllers/authController.js
+++ Nodejs/mongodb-node-1/New_Implementation/controllers/authController.js
@@ -1,10 +1,17 @@
-import * as HttpStatus from 'http-status-codes'
+import UserModel from '../models/UserModel'
 
 import asyncHandler from '../utils/asyncHandler'
 import Response from '../utils/response'
 
-const registerUser = asyncHandler((req, res, next) => {
-    Response.success(res, 'Route working successfully')
+const registerUser = asyncHandler(async (req, res, next) => {
+    await UserModel.create({
+        name: req.body['name'],
+        email: req.body['email'],
+        password: req.body['password'],
+        passwordConfirm: req.body['passwordConfirm'],
+        photo: req.body['photo'],
+    })
+    return Response.success(res, 'User account created successfully !')
 })
 
 const loginUser = asyncHandler((req, res, next) => {
