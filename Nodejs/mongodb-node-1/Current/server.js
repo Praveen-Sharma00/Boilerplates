606588a4f2bdfafd5a4c99072a9e6d305a91e711
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-16 17:05:28 +0530

    Server setup done

---
+++ Nodejs/mongodb-node-1/Current/server.js
@@ -0,0 +1,29 @@
+import {config} from 'dotenv'
+import mongoose from 'mongoose'
+
+(async ({env}) => {
+    config({
+        path: './config.env'
+    })
+
+    const DB_URI = env.NODE_ENV === 'development' ? env.DB_LOCAL_URI : env.DB_PROD_URI
+    try {
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        })
+        console.log('DB connection established !')
+    } catch (err) {
+        console.log('Error connecting to database !')
+        process.exit(1)
+    }
+})(process)
+
+
+import app from './app'
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening on port ' + process.env.PORT)
+})
+
