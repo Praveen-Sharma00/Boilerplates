8d81f4b96d724b196fce4bfc31fc44b05da7720d
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-20 17:14:48 +0530

    Implemented loginUser  , token essential functions

--- Nodejs/mongodb-node-1/Current/controllers/authController.js
+++ Nodejs/mongodb-node-1/Current/controllers/authController.js
@@ -1,6 +1,31 @@
-import UserModel from '../models/UserModel'
+import jwt from 'jsonwebtoken'
 
+import UserModel from '../models/UserModel'
 import asyncHandler from '../utils/asyncHandler'
+import AppError from '../utils/AppError'
+
+const signToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
+const createAndSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions)
+    user.password = undefined
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
 
 const registerUser = asyncHandler(async (req, res, next) => {
     const newUser = new UserModel({
@@ -15,8 +40,19 @@ const registerUser = asyncHandler(async (req, res, next) => {
         message: 'done'
     })
 })
+const loginUser = asyncHandler(async (req, res, next) => {
+    let {email, password} = req.body
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password !', 400))
+    }
+    const user = await UserModel.findOne({email}).select('+password')
+    if (!user || await user.correctPassword(password, user.password)) {
+        return next(new AppError('Incorrect email or password !', 401))
+    }
+    createAndSendToken(user, 200, res)
+})
 
 const AuthController = {
-    registerUser,
+    registerUser, loginUser
 }
 export default AuthController
