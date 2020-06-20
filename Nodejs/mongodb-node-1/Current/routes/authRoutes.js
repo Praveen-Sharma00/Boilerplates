d80fbbec8b9b4344572f27a4be20155fe3f59875
Praveen Sharma <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-06-20 17:16:24 +0530

    Added 'login' route

--- Nodejs/mongodb-node-1/Current/routes/authRoutes.js
+++ Nodejs/mongodb-node-1/Current/routes/authRoutes.js
@@ -6,4 +6,7 @@ const authRouter = express.Router()
 authRouter.route('/register')
     .post(AuthController.registerUser)
 
+authRouter.route('/login')
+    .post(AuthController.loginUser)
+
 export default authRouter
